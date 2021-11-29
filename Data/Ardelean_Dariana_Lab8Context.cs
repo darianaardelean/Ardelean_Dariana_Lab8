a48f740ca19e54ebb00b689e2c1c8012978e79aa
Dariana Ardelean <ardelean.dariana05@gmail.com>
2021-11-29 17:26:26 +0200

    Add project files.

---
+++ Data/Ardelean_Dariana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ardelean_Dariana_Lab8.Models;
+
+namespace Ardelean_Dariana_Lab8.Data
+{
+    public class Ardelean_Dariana_Lab8Context : DbContext
+    {
+        public Ardelean_Dariana_Lab8Context (DbContextOptions<Ardelean_Dariana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ardelean_Dariana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Ardelean_Dariana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Ardelean_Dariana_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ardelean_Dariana_Lab8.Data;
+using Ardelean_Dariana_Lab8.Models;
+
+namespace Ardelean_Dariana_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ardelean_Dariana_Lab8.Data.Ardelean_Dariana_Lab8Context _context;
+
+        public IndexModel(Ardelean_Dariana_Lab8.Data.Ardelean_Dariana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+            
+        }
+    }
+}
